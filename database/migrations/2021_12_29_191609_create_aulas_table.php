6915a0293673cb00ddb686a6f6cd621c11416c58
Phelipe Stering <phelipe.stering@hotmail.com>
2021-12-31 09:56:00 -0300

    AULA: 06 - LARAVEL RELACIONAMENTO ONE TO MANY (E INVERSO) - CONSTRUINDO FILLABLES

---
+++ database/migrations/2021_12_29_191609_create_aulas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAulasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aulas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('modulo_id')->constrained('modulos');
+            $table->string('nome');
+            $table->string('video');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aulas');
+    }
+}
